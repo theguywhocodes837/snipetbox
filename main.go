d74a7aae5d6a84a66a5d84ad76f6b546ebc46f25
Vaibhav Jha <vaibhavjha837@gmail.com>
2021-04-18 22:50:29 +0530

    throw 405 for non POST method createMethod

--- main.go
+++ main.go
@@ -6,12 +6,37 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from snipetbox"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		w.Header().Set("Cache-Control", "ppublic")
+		w.Header().Set("Cache-Control", "max-age=273727448")
+		http.Error(w, "Method Not Allowed", 405)
+		return
+	}
+	//w.Write([]byte("Display a specific snippet..."))
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{"name": "Alex"}`))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
