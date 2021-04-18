65e6dd63b764528158f0a533ac1fc74e3146637e
Vaibhav Jha <vaibhavjha837@gmail.com>
2021-04-18 23:36:48 +0530

    change project structure

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -15,7 +16,14 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
@@ -30,15 +38,4 @@ func createSnippet(w http.ResponseWriter, r *http.Request) {
 	//w.Write([]byte("Display a specific snippet..."))
 	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte(`{"name": "Alex"}`))
-}
-
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
-
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
 }
